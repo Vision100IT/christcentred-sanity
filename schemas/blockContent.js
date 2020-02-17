9a22a80ab313ba9d0f50a42c3ddebccda5fa49e4
Bratalix <bratalix@gmail.com>
2020-02-17 11:00:15 +1100

    Validation for pages

--- schemas/blockContent.js
+++ schemas/blockContent.js
@@ -13,10 +13,12 @@ export default {
   title: 'Block Content',
   name: 'blockContent',
   type: 'array',
+  validation: Rule => Rule.required(),
   of: [
     {
       title: 'Block',
       type: 'block',
+      
       // Styles let you set what your user can mark up blocks with. These
       // corrensponds with HTML tags, but you can set any title or value
       // you want and decide how you want to deal with it where you want to
